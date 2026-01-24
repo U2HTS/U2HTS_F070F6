0a17d9a59c3db47c2d3a354003db36b8d68e7047
CNflysky <cnflysky@qq.com>
2026-01-24 22:13:35 +0800

    update gpio i2c

--- Core/Inc/gpio_i2c.h
+++ Core/Inc/gpio_i2c.h
@@ -18,5 +18,6 @@ typedef struct {
 bool gpio_i2c_write(gpio_i2c* i2c, uint8_t slave_addr, void* buf, size_t len,
                     bool stop);
 bool gpio_i2c_read(gpio_i2c* i2c, uint8_t slave_addr, void* buf, size_t len);
+void gpio_i2c_reset_bus(gpio_i2c* i2c);
 bool gpio_i2c_detect_slave(gpio_i2c* i2c, uint8_t addr);
 #endif
--- Core/Src/gpio_i2c.c
+++ Core/Src/gpio_i2c.c
@@ -1,5 +1,5 @@
 #include "gpio_i2c.h"
-static void gpio_i2c_start(gpio_i2c *i2c) {
+static void gpio_i2c_start(gpio_i2c* i2c) {
   i2c->setsda(1);
   i2c->setscl(1);
   i2c->delay_us(i2c->delay / 2);
@@ -8,7 +8,7 @@ static void gpio_i2c_start(gpio_i2c *i2c) {
   i2c->setscl(0);
 }
 
-void gpio_i2c_stop(gpio_i2c *i2c) {
+void gpio_i2c_stop(gpio_i2c* i2c) {
   i2c->setsda(0);
   i2c->delay_us(i2c->delay / 2);
   i2c->setscl(1);
@@ -17,7 +17,7 @@ void gpio_i2c_stop(gpio_i2c *i2c) {
   i2c->delay_us(i2c->delay);
 }
 
-static void gpio_i2c_scl_high_wait(gpio_i2c *i2c) {
+static void gpio_i2c_scl_high_wait(gpio_i2c* i2c) {
   i2c->setscl(1);
   i2c->delay_us(i2c->delay / 2);
   uint16_t timeout = 0xFFFF;
@@ -26,7 +26,7 @@ static void gpio_i2c_scl_high_wait(gpio_i2c *i2c) {
 }
 
 // true: ack false: nack
-static void gpio_i2c_ack(gpio_i2c *i2c, bool ack) {
+static void gpio_i2c_ack(gpio_i2c* i2c, bool ack) {
   i2c->setscl(0);
   i2c->setsda(!ack);
   i2c->delay_us(i2c->delay);
@@ -35,12 +35,13 @@ static void gpio_i2c_ack(gpio_i2c *i2c, bool ack) {
   i2c->setsda(1);
 }
 
-static bool gpio_i2c_wait_ack(gpio_i2c *i2c) {
+static bool gpio_i2c_wait_ack(gpio_i2c* i2c) {
   i2c->setsda(1);
   i2c->delay_us(i2c->delay / 2);
   i2c->setscl(1);
   i2c->delay_us(i2c->delay / 2);
   uint8_t timeout = 0;
+  i2c->setsdamode(false);
   while (i2c->getsda()) {
     timeout++;
     if (timeout > 254) {
@@ -49,12 +50,13 @@ static bool gpio_i2c_wait_ack(gpio_i2c *i2c) {
     }
     i2c->delay_us(1);
   }
+  i2c->setsdamode(true);
   i2c->delay_us(i2c->delay);
   i2c->setscl(0);
   return true;
 }
 
-static void gpio_i2c_write_byte(gpio_i2c *i2c, uint8_t byte) {
+static void gpio_i2c_write_byte(gpio_i2c* i2c, uint8_t byte) {
   for (uint8_t i = 0; i < 8; i++) {
     i2c->setscl(0);
     i2c->setsda((byte & 0x80) ? 1 : 0);
@@ -67,7 +69,7 @@ static void gpio_i2c_write_byte(gpio_i2c *i2c, uint8_t byte) {
   i2c->delay_us(i2c->delay);
 }
 
-static uint8_t gpio_i2c_read_byte(gpio_i2c *i2c, bool ack) {
+static uint8_t gpio_i2c_read_byte(gpio_i2c* i2c, bool ack) {
   uint8_t buf = 0x00;
   i2c->setsdamode(false);
   for (uint8_t i = 0; i < 8; i++) {
@@ -84,7 +86,8 @@ static uint8_t gpio_i2c_read_byte(gpio_i2c *i2c, bool ack) {
   return buf;
 }
 
-bool gpio_i2c_write(gpio_i2c *i2c, uint8_t slave_addr, void* buf, size_t len, bool stop) {
+bool gpio_i2c_write(gpio_i2c* i2c, uint8_t slave_addr, void* buf, size_t len,
+                    bool stop) {
   uint8_t* buf_ptr = buf;
   bool ret = false;
   gpio_i2c_start(i2c);
@@ -100,20 +103,39 @@ bool gpio_i2c_write(gpio_i2c *i2c, uint8_t slave_addr, void* buf, size_t len, bo
   return ret;
 }
 
-bool gpio_i2c_read(gpio_i2c *i2c, uint8_t slave_addr, void* buf, size_t len) {
+bool gpio_i2c_read(gpio_i2c* i2c, uint8_t slave_addr, void* buf, size_t len) {
   uint8_t* buf_ptr = buf;
   bool ret = false;
   gpio_i2c_start(i2c);
   gpio_i2c_write_byte(i2c, (slave_addr << 1) | 1);
   ret = gpio_i2c_wait_ack(i2c);
   if (!ret) return ret;
-  for (uint32_t i = 0; i < len; i++) buf_ptr[i] = gpio_i2c_read_byte(i2c, (i != len - 1));
+  for (uint32_t i = 0; i < len; i++)
+    buf_ptr[i] = gpio_i2c_read_byte(i2c, (i != len - 1));
 
   gpio_i2c_stop(i2c);
   return ret;
 }
 
+void gpio_i2c_reset_bus(gpio_i2c* i2c) {
+  i2c->setsda(1);
+  i2c->setscl(1);
+  i2c->delay_us(i2c->delay);
+  for (uint8_t i = 0; i < 9; i++) {
+    i2c->setscl(0);
+    i2c->delay_us(i2c->delay);
+    i2c->setscl(1);
+    i2c->delay_us(i2c->delay);
+  }
+
+  gpio_i2c_stop(i2c);
+}
+
 bool gpio_i2c_detect_slave(gpio_i2c* i2c, uint8_t addr) {
+  gpio_i2c_stop(i2c);
+  i2c->delay_us(200);
+  gpio_i2c_reset_bus(i2c);
+
   gpio_i2c_start(i2c);
   gpio_i2c_write_byte(i2c, addr << 1);
 
@@ -121,12 +143,12 @@ bool gpio_i2c_detect_slave(gpio_i2c* i2c, uint8_t addr) {
   i2c->delay_us(i2c->delay);
   i2c->setscl(true);
   i2c->delay_us(i2c->delay);
-
+  i2c->setsdamode(false);
   bool ack = !i2c->getsda();
-
+  i2c->setsdamode(true);
   i2c->delay_us(i2c->delay);
   i2c->setscl(false);
   gpio_i2c_stop(i2c);
-  
+
   return ack;
 }
